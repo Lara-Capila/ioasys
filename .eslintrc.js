548cfa6cbe7870ee1112db0e415302a3a712d02c
Lara-Capila <laracapilak@gmail.com>
2021-12-14 22:07:13 -0300

    config(): Configurando eslint e prettier

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["trybe-frontend", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "react/jsx-curly-spacing": [2, { when: "never", allowMultiline: false }],
+    "jsx-a11y/label-has-associated-control": [
+      2,
+      {
+        assert: "either", // either check for `htmlFor` or `nesting`
+      },
+    ],
+  },
+};
